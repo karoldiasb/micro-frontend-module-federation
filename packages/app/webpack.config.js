8fda47e97ab961a9f28115b4aceb7bcf567e09e7
Karoline Barreto <karolinedbarreto@gmail.com>
2023-12-14 23:59:34 -0300

    chore: add eslint and prettier configuration

--- packages/app/webpack.config.js
+++ packages/app/webpack.config.js
@@ -1,43 +1,43 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+// const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-    entry: '.src/index.js',
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, './dist'),
-        publicPath: 'http://localhost:9001/'
+  entry: './src/index.js',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.jsx', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: require.resolve('babel-loader'),
+        options: {
+          presets: [require.resolve('@babel/preset-react')],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new ESLintPlugin({
+      extensions: ['js', 'jsx'],
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, './dist'),
     },
-    devServer: {
-        contentBase: path.resolve(__dirname, './dist'),
-        index: 'index.html',
-        port: 9001,
-        historyFallbackApi: true
-    },
-    resolve: {
-        extensions: ['.jsx', '.js', '.json']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                loader: require.resolve('babel-loader'),
-                options: {
-                    presets: [require.resolve('@babel/preset-react')]
-                }
-            },
-            {
-                test: /\.css?$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename:'index.html',
-            template: './public/index.html',
-            title: 'App'
-        })
-    ]
-}
+    port: 3000,
+    historyApiFallback: true,
+  },
+};
